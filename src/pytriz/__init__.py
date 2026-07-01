fcd30f1f057e2246fd3999d4ad48a6317ddc4830
mmysior <mysior.marek@gmail.com>
2026-07-01 22:09:34 +0200

    Remove unused imports and update __all__ in __init__.py

--- src/pytriz/__init__.py
+++ src/pytriz/__init__.py
@@ -1,9 +1,5 @@
-from pydantic_ai import Embedder
-from pydantic_ai.models import Model
-from pydantic_ai.settings import ModelSettings
-
 from . import contradictions
 from .core.providers import get_embedder, get_model
 from .store import TRIZStore
 
-__all__ = ["contradictions", "Embedder", "get_embedder", "Model", "get_model", "ModelSettings", "TRIZStore"]
+__all__ = ["contradictions", "get_embedder", "get_model", "TRIZStore"]
